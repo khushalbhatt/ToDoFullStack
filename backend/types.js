d700f6a7df22759c23d972c43bc9db8e1e572378
Khushal Kumar <65487241+khushalbhatt@users.noreply.github.com>
2025-02-16 12:40:24 +0530

    Update types.js

--- backend/types.js
+++ backend/types.js
@@ -3,12 +3,10 @@ const zod = require('zod');
 const createTodoSchema = zod.object({
     title : zod.string(),
     description: zod.string(),
-    done : zod.boolean()
 })
 
 const updateTodoSchema = zod.object({
     id : zod.string(),
-    done : zod.boolean()
 })
 
 module.exports = {
