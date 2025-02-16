94662a6396c286d5e0945140649f4d3c1b727502
Khushal Kumar <khushalbhatt17@gmail.com>
2025-02-16 12:04:54 +0530

    third push

--- index.js
+++ index.js
@@ -2,27 +2,37 @@ const express = require('express');
 const app = express();
 app.use(express.json());
 const { createTodoSchema, updateTodoSchema } = require('./types.js');
+const { Todo } = require('./db.js');
 
-
-app.get('/todos', (req, res) => {
-    res.send('Hello World');
+app.get('/todos', async (req, res) => {
+    const todos = await Todo.find({});
 })  
 
-app.post('/todos', (req, res) => {
+app.post('/todos', async (req, res) => {
    const createPayload = req.body;
    const parsedPayload = createTodoSchema.safeParse(createPayload);
    if(parsedPayload.success){
-       res.send(parsedPayload.data);
+      await Todo.create({
+           title: createPayload.title,
+           description: createPayload.description,
+           done: false
+       })
+       res.send("Todo Created");
    } else {
     res.status(411).send("Wrong Payload");
    }
 })
 
-app.put('/completed', (req, res) => {
+app.put('/completed',async (req, res) => {
    const updatePayload = req.body;
    const parsedPayload = updateTodoSchema.safeParse(updatePayload);
    if(parsedPayload.success){
-       res.send(parsedPayload.data);
+        await Todo.update({
+            _id: req.body.id
+        },{
+            done: true
+        })
+        res.send("Todo is Updated");        
    } else {
     res.status(411).send("Wrong Payload");
    }
