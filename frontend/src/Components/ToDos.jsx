b901bcda1b1e1d992deac181b0584d4572628d8b
Khushal Kumar <khushalbhatt17@gmail.com>
2025-02-18 00:55:33 +0530

    Updating the mark as completed for a particular block

--- frontend/src/Components/ToDos.jsx
+++ frontend/src/Components/ToDos.jsx
@@ -1,4 +1,17 @@
 export function ToDos(todos){
+    async function updateValue(todoid){
+        const updatePayload = {
+            id: todoid
+        }
+        const response = await fetch('http://localhost:3000/completed',{
+            method: 'PUT',
+            body: JSON.stringify(updatePayload),
+            headers: {
+                'Content-Type': 'application/json'
+            }
+        })
+        console.log(response);
+    }
     return(
         <div>
             {
@@ -7,7 +20,7 @@ export function ToDos(todos){
                         <div>
                             <h1>{todo.title}</h1>
                             <p>{todo.description}</p>
-                            <button>{todo.completed == true ? "Completed" : "Mark as Complete"}</button>
+                            <button onClick={()=>updateValue(todo._id)}>{todo.done == true ? "Completed" : "Mark as Complete"}</button>
                         </div>
                     )
                 })
